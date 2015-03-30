518e69a105f61ff4d18e79830765c66b1fd789bb
Ömer Eryılmaz <omreryilmaz@gmail.com>
2015-03-30 11:14:31 +0300

    Array Based

---
+++ ArrayBased_AA30Mart/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBased_AA30Mart
+{
+    public class ItsAlmostAStack<T>
+    {
+
+        private List<T> items = new List<T>();
+        int size = 0;
+
+        public void Push(T item)
+        {
+            items.Add(item);
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (items.Count > 0)
+            {
+                T temp = items[items.Count - 1];
+                items.RemoveAt(items.Count - 1);
+                return temp;
+            }
+            else
+                return default(T);
+        }
+
+        public void Remove(int itemAtPosition)
+        {
+            items.RemoveAt(itemAtPosition);
+            size--;
+        }
+
+        public int listSize()
+        {
+            return size;
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            ItsAlmostAStack<int> stk = new ItsAlmostAStack<int>();
+            stk.Push(15);
+            stk.Push(18);
+
+            Console.WriteLine("Silmeden once boyut : "+ stk.listSize());
+
+            stk.Remove(1);
+
+            Console.WriteLine("Sildikten sonra boyut : " + stk.listSize());
+
+            Console.WriteLine(stk.Pop());
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ArrayBased_AA30Mart/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArrayBased_AA30Mart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArrayBased_AA30Mart")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ced98952-fa66-4c89-8f2a-97d4bd9eecdf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
